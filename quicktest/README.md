9b31aa7fa95740ce6adefd7a58d6fb70498c88f2
Metroynome <yort124@gmail.com>
2023-11-15 21:55:08 -0500

    added quicktest

---
+++ quicktest/README.md
@@ -0,0 +1,2 @@
+# quick test
+uses a different hook if I need to quickly share something with someone
---
+++ quicktest/main.c
@@ -0,0 +1,44 @@
+#include <tamtypes.h>
+
+#include <libuya/stdio.h>
+#include <libuya/string.h>
+#include <libuya/player.h>
+#include <libuya/utils.h>
+#include <libuya/game.h>
+#include <libuya/pad.h>
+#include <libuya/uya.h>
+#include <libuya/weapon.h>
+#include <libuya/interop.h>
+#include <libuya/moby.h>
+#include <libuya/graphics.h>
+#include <libuya/gamesettings.h>
+#include <libuya/spawnpoint.h>
+#include <libuya/team.h>
+#include <libuya/ui.h>
+#include <libuya/time.h>
+
+int main()
+{
+	uyaPreUpdate();
+
+	GameSettings * gameSettings = gameGetSettings();
+	GameOptions * gameOptions = gameGetOptions();
+	if (gameOptions || gameSettings || gameSettings->GameLoadStartTime > 0)
+	{
+
+	}
+
+    if (isInGame())
+    {
+        Player * p = (Player*)PLAYER_STRUCT;
+		if (!p)
+			return 0;
+
+        int state = playerDeobfuscate(&p->State, 0, 0);
+		printf("\nState: %d", state);
+    }
+	
+	uyaPostUpdate();
+
+    return 0;
+}
