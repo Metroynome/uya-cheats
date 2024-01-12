e8bb8f1257acb7508dcff90b4efcf83bb1f78052
Metroynome <yort124@gmail.com>
2024-01-12 13:40:12 -0500

    updates

--- test/main.c
+++ test/main.c
@@ -33,6 +33,8 @@ static int EFFECT_ME = 21;
 static int SOUND_ME = 0;
 static int SOUND_ME_FLAG = 3;
 
+extern VariableAddress_t vaFlagUpdate_Func;
+
 void DebugInGame(Player* player)
 {
     if (playerPadGetButtonDown(player, PAD_LEFT) > 0) {
@@ -315,15 +317,302 @@ void drawSomething(Moby* moby)
 	drawEffectQuad(pos, EFFECT_ME, 2);
 }
 
-void Test()
+
+
+struct FlagPVars
 {
+	VECTOR BasePosition;
+	short CarrierIdx;
+	short LastCarrierIdx;
+	short Team;
+	char UNK_16[6];
+	int TimeFlagDropped;
+};
+/*
+ * NAME :		flagHandlePickup
+ * 
+ * DESCRIPTION :
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void flagHandlePickup(Moby* flagMoby, int pIdx)
+{
+	Player** players = playerGetAll();
+	Player* player = players[pIdx];
+	if (!player || !flagMoby)
+		return;
+	
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	// fi flag state isn't 1
+	if (flagMoby->State != 1)
+		return;
+
+	// flag is currently returning
+	if (flagIsReturning(flagMoby))
+		return;
+
+	// flag is currently being picked up
+	if (flagIsBeingPickedUp(flagMoby))
+		return;
+
+	// only allow actions by living players
+	if (playerIsDead(player) || playerGetHealth(player) <= 0)
+		return;
+
+	// Handle pickup/return
+	if (player->mpTeam == pvars->Team) {
+		flagReturnToBase(flagMoby, 0, pIdx);
+	} else {
+		flagPickup(flagMoby, pIdx);
+		player->FlagMoby = flagMoby;
+	}
+	DPRINTF("player %d picked up flag %X at %d\n", player->mpIndex, flagMoby->OClass, gameGetTime());
+}
+
+/*
+ * NAME :		flagRequestPickup
+ * 
+ * DESCRIPTION :
+ * 			Requests to either pickup or return the given flag.
+ * 			If host, this request is automatically passed to the handler.
+ * 			If not host, this request is sent over the net to the host.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void flagRequestPickup(Moby* flagMoby, int pIdx)
+{
+	static int requestCounters[GAME_MAX_PLAYERS] = {0,0,0,0,0,0,0,0,0,0};
+	Player** players = playerGetAll();
+	Player* player = players[pIdx];
+	if (!player || !flagMoby)
+		return;
+	
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	if (gameAmIHost()) {
+		// handle locally
+		flagHandlePickup(flagMoby, pIdx);
+	} else if (requestCounters[pIdx] == 0) {
+		// send request to host
+		// void* dmeConnection = netGetDmeServerConnection();
+		// if (dmeConnection) {
+		// 	ClientRequestPickUpFlag_t msg;
+		// 	msg.GameTime = gameGetTime();
+		// 	msg.PlayerId = player->mpIndex;
+		// 	msg.FlagUID = guberGetUID(flagMoby);
+		// 	netSendCustomAppMessage(dmeConnection, gameGetHostId(), CUSTOM_MSG_ID_FLAG_REQUEST_PICKUP, sizeof(ClientRequestPickUpFlag_t), &msg);
+		// 	requestCounters[pIdx] = 10;
+		// 	DPRINTF("sent request flag pickup %d\n", gameGetTime());
+		// }
+	}
+	else
+	{
+		requestCounters[pIdx]--;
+	}
+}
+
+/*
+ * NAME :		customFlagLogic
+ * 
+ * DESCRIPTION :
+ * 			Reimplements flag pickup logic but runs through our host authoritative logic.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Troy "Metroynome" Pruitt
+ */
+void customFlagLogic(Moby* flagMoby)
+{
+	VECTOR t;
+	int i;
+	Player** players = playerGetAll();
+	int gameTime = gameGetTime();
+	GameOptions* gameOptions = gameGetOptions();
+
+	// if not in game
+	if (!isInGame())
+		return;
+
+	// if flagMoby doesn't exist
+	if (!flagMoby)
+		return;
+
+	// if flag pvars don't exist
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	// if flag state doesn't equal 1
+	if (flagMoby->State != 1)
+		return;
+
+	// if flag is returning
+	if (flagIsReturning(flagMoby))
+		return;
+
+	// if flag is being picked up
+	if (flagIsBeingPickedUp(flagMoby))
+		return;
+
+	// return to base if flag has been idle for 40 seconds
+	if ((pvars->TimeFlagDropped + (TIME_SECOND * 40)) < gameTime && !flagIsAtBase(flagMoby)) {
+		flagReturnToBase(flagMoby, 0, 0xFF);
+		return;
+	}
+	
+	// if flag didn't land on safe ground, and after .5s a player died
+	static int flagIgnorePlayer = 0;
+	// if(gameConfig.grFlagHotspots) {
+	// 	if (!flagIsOnSafeGround(flagMoby) && !flagIsAtBase(flagMoby) && (flagIgnorePlayer + 300) < gameTime) {
+	// 		flagReturnToBase(flagMoby, 0, 0xff);
+	// 		return;
+	// 	}
+	// }
+
+	// wait 1.5 seconds for last carrier to be able to pick up again
+	if ((pvars->TimeFlagDropped + 1500) > gameTime)
+		return;
+
+	for (i = 0; i < GAME_MAX_PLAYERS; ++i) {
+		Player* player = players[i];
+		if (!player || !player->IsLocal)
+			continue;
+
+		// only allow actions by living players, and non-chargebooting players
+		if ((playerIsDead(player) || playerGetHealth(player) <= 0) || (player->timers.IsChargebooting == 1 && (playerPadGetButton(player, PAD_R2) > 0) && player->timers.state > 55)){
+			// if flag holder died, update flagIgnorePlayer time.
+			if (pvars->LastCarrierIdx == player->mpIndex)
+				flagIgnorePlayer = gameTime;
+
+			continue;
+		}
+
+		// skip player if they've only been alive for < 3 seconds
+		if (player->timers.timeAlive <= 180)
+			continue;
+
+		// skip player if in vehicle
+		if (player->Vehicle && playerDeobfuscate(&player->State, 0, 0) == PLAYER_STATE_VEHICLE)
+			continue;
+
+		// skip if player state is in vehicle and critterMode is on
+		if (player->Camera && player->Camera->camHeroData.critterMode)
+			continue;
+
+		// skip if player is on teleport pad
+		// AQuATOS BUG: player->ground.pMoby points to wrong area
+		if (player->ground.pMoby && player->ground.pMoby->OClass == MOBY_ID_TELEPORT_PAD)
+			continue;
+
+		// player must be within 2 units of flag
+		vector_subtract(t, flagMoby->Position, player->PlayerPosition);
+		float sqrDistance = vector_sqrmag(t);
+		if (sqrDistance > (2*2))
+			continue;
+
+		// player is on different team than flag and player isn't already holding flag
+		if (player->mpTeam != pvars->Team) {
+			if (!player->FlagMoby) {
+				flagRequestPickup(flagMoby, i);
+				return;
+			}
+		} else {
+			// if player is on same team as flag and close enough to return it
+			vector_subtract(t, pvars->BasePosition, flagMoby->Position);
+			float sqrDistanceToBase = vector_sqrmag(t);
+			if (sqrDistanceToBase > 0.1) {
+				flagRequestPickup(flagMoby, i);
+				return;
+			}
+		}
+	}
+}
+
+/*
+ * NAME :		patchCTFFlag
+ * 
+ * DESCRIPTION :
+ * 			Patch CTF Flag update function with our own
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Troy "Metroynome" Pruitt
+ */
+void patchCTFFlag(void)
+{
+	VECTOR t;
+	int i = 0;
+	Player** players = playerGetAll();
+
+	if (!isInGame())
+		return;
+
+	// netInstallCustomMsgHandler(CUSTOM_MSG_ID_FLAG_REQUEST_PICKUP, &onRemoteClientRequestPickUpFlag);
+
+	u32 FlagFunc = GetAddress(&vaFlagUpdate_Func);
+	if (FlagFunc){
+		*(u32*)FlagFunc = 0x03e00008;
+		*(u32*)(FlagFunc + 0x4) = 0x0000102D;
+	}
+
+	GuberMoby* gm = guberMobyGetFirst();
+	while (gm) {
+		if (gm->Moby) {
+			switch (gm->Moby->OClass) {
+				case MOBY_ID_CTF_RED_FLAG:
+				case MOBY_ID_CTF_BLUE_FLAG:
+				{
+					customFlagLogic(gm->Moby);
+					// Master * master = masterGet(gm->Guber.Id.UID);
+					// if (master)
+					// 	masterDelete(master);
+
+					break;
+				}
+			}
+		}
+		gm = (GuberMoby*)gm->Guber.Next;
+	}
+}
+
+int main(void)
+{
+	((void (*)(void))0x00126780)();
+
+	uyaPreUpdate();
+
 	GameSettings * gameSettings = gameGetSettings();
 	GameOptions * gameOptions = gameGetOptions();
-	if (gameOptions || gameSettings || gameSettings->GameLoadStartTime > 0) {
-		gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_SmallTurrets = 0;
-		gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_Bots = 0;
-		gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_GatlinTurrets = 0;
-	}
+	// if (gameOptions || gameSettings || gameSettings->GameLoadStartTime > 0) {
+	// 	gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_SmallTurrets = 0;
+	// 	gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_Bots = 0;
+	// 	gameOptions->GameFlags.MultiplayerGameFlags.BaseDefense_GatlinTurrets = 0;
+	// }
 
     if (isInGame()) {
 		Player * p = playerGetFromSlot(0);
@@ -365,11 +654,16 @@ void Test()
 		// printf("Collision: %d\n", CollHotspot());
         
 		// drawCollider(p->PlayerMoby);
-        runB6HitVisualizer();
+		patchCTFFlag();
+        // runB6HitVisualizer();
 		InfiniteChargeboot();
 		InfiniteHealthMoonjump();
     	DebugInGame(p);
     } else {
 		DebugInMenus();
 	}
+
+	uyaPostUpdate();
+
+	return 0;
 }
