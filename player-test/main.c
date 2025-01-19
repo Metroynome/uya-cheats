1bfce0a73032b58ce72ae7c51c54a75013a2fc21
Metroynome <yort124@gmail.com>
2025-01-18 19:27:26 -0500

    Added player-test

---
+++ player-test/main.c
@@ -0,0 +1,143 @@
+#include <tamtypes.h>
+
+#include <libuya/stdio.h>
+#include <libuya/string.h>
+#include <libuya/player.h>
+#include <libuya/utils.h>
+#include <libuya/game.h>
+#include <libuya/pad.h>
+#include <libuya/uya.h>
+#include <libuya/weapon.h>
+#include <libuya/interop.h>
+#include <libuya/moby.h>
+#include <libuya/graphics.h>
+#include <libuya/gamesettings.h>
+#include <libuya/spawnpoint.h>
+#include <libuya/team.h>
+#include <libuya/ui.h>
+#include <libuya/time.h>
+#include <libuya/camera.h>
+#include <libuya/gameplay.h>
+#include <libuya/map.h>
+#include <libuya/collision.h>
+#include <libuya/guber.h>
+#include <libuya/sound.h>
+
+void DebugInGame(Player* player)
+{
+    if (playerPadGetButtonDown(player, PAD_LEFT) > 0) {
+		// Swap Teams
+		int SetTeam = (player->mpTeam < 7) ? player->mpTeam + 1 : 0;
+		playerSetTeam(player, SetTeam);
+	} else if (playerPadGetButtonDown(player, PAD_RIGHT) > 0) {
+        // Hurt Player
+        playerDecHealth(player, 1);
+	} else if (playerPadGetButtonDown(player, PAD_UP) > 0) {
+		// nothing
+	} else if(playerPadGetButtonDown(player, PAD_DOWN) > 0) {
+		// Set Gattling Turret Health to 1.
+		DEBUGsetGattlingTurretHealth();
+	} else if (playerPadGetButtonDown(player, PAD_L3) > 0) {
+		// Nothing Yet!
+	} else if (playerPadGetButtonDown(player, PAD_R3) > 0) {
+		// do nothing
+	}
+}
+
+void DebugInMenus(void)
+{
+    if (padGetButtonDown(0, PAD_LEFT) > 0) {
+		// Nothing Yet!
+	} else if (padGetButtonDown(0, PAD_RIGHT) > 0) {
+		// Nothing Yet!
+	} else if (padGetButtonDown(0, PAD_UP) > 0) {
+		// Nothing Yet!
+	} else if(padGetButtonDown(0, PAD_DOWN) > 0) {
+		// Nothing Yet!
+	} else if (padGetButtonDown(0, PAD_L3) > 0) {
+		// Nothing Yet!
+	} else if (padGetButtonDown(0, PAD_R3) > 0) {
+		// Nothing Yet!
+	}
+}
+
+void InfiniteChargeboot(void)
+{
+	int i;
+	Player ** players = playerGetAll();
+	for (i = 0; i < GAME_MAX_PLAYERS; ++i)
+	{
+		Player * player = players[i];
+		if (!player)
+			continue;
+
+		if (player->timers.IsChargebooting == 1 && playerPadGetButton(player, PAD_R2) > 0 && player->timers.state > 55)
+			player->timers.state = 55;
+	}
+}
+
+void InfiniteHealthMoonjump(void)
+{
+    static int _InfiniteHealthMoonjump_Init = 0;
+    int Joker = *(u16*)0x00225982;
+    if (Joker == 0xFDFB)
+        _InfiniteHealthMoonjump_Init = 1;
+    else if (Joker == 0xFFFD)
+        _InfiniteHealthMoonjump_Init = 0;
+
+    if (_InfiniteHealthMoonjump_Init)
+    {
+        Player * player = (Player*)PLAYER_STRUCT;
+		if (playerGetHealth(player) < 15)
+        	playerSetHealth(player, 15);
+
+        if (Joker == 0xBFFF)
+            (float)player->move.behavior[2] = 0.125;
+    }
+}
+
+void DEBUGsetGattlingTurretHealth(void)
+{
+    Moby* moby = mobyListGetStart();
+    // Iterate through mobys and change health
+    while ((moby = mobyFindNextByOClass(moby, MOBY_ID_GATLING_TURRET))) {
+        if (moby->pVar) {
+			*(float*)((u32)moby->pVar + 0x30) = 0;
+        }
+        ++moby;
+    }
+}
+
+int main(void)
+{
+	((void (*)(void))0x00126780)();
+
+	uyaPreUpdate();
+
+    if (isInGame()) {
+		Player * p = playerGetFromSlot(0);
+		if (!p)
+			return 0;
+
+		// Force Normal Up/Down Controls
+		*(u32*)0x001A5A70 = 0;
+
+		// Only print state if it's diferent from last state.
+		static int nowState = -1;
+		int currentState = playerDeobfuscate(&p->state, 0, 0);
+		if (currentState != nowState) {
+			nowState = currentState;
+			// printf("\nState: %d", nowState);
+		}
+
+		InfiniteChargeboot();
+		InfiniteHealthMoonjump();
+    	DebugInGame(p);
+    } else {
+		// DebugInMenus();
+	}
+
+	uyaPostUpdate();
+
+	return 0;
+}
